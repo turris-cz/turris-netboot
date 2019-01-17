886e0339a3766f54696a547d58e051e86782d758
Michal Hrusecky <michal.hrusecky@nic.cz>
2019-01-17 10:35:06 +0100

    Fix initial setup

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@ mkdir -p /srv/tftp
 uci commit
 /etc/init.d/dnsmasq restart
 grep -q '^turris-netboot:' /etc/passwd || useradd -md /srv/turris-netboot -s /bin/ash turris-netboot
-passwd -u turris-netboot
+usermod -p "$(head -c 100 /dev/urandom | hexdump -e '"%x"')" turris-netboot
+usermod -U turris-netboot
 chown -R turris-netboot /srv/turris-netboot
 mkdir -p /srv/tftp/pxelinux.cfg
 touch /srv/tftp/pxelinux.cfg/default-arm-mvebu-turris_mox
 chown turris-netboot /srv/tftp/pxelinux.cfg/default-arm-mvebu-turris_mox
 mkdir -p /srv/tftp/turris-netboot
 chown turris-netboot /srv/tftp/turris-netboot
-su - turris-netboot netboot-manage regen
+su - turris-netboot netboot-manager regen
